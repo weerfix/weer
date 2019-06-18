bf12ecb8170644b39705dc471748d49151c1b987
guolf <guoli1feng@163.com>
2019-06-18 19:56:00 +0800

    新增Sample示例代码

---
+++ Sample/CoreTest/Controllers/SimpolesController.cs
@@ -0,0 +1,23 @@
+﻿using CoreTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTest.Controllers
+{
+    public class SimpolesController: Controller
+    {
+        public IActionResult Index()
+
+        {
+            var simple = new List<Simpoles>();
+            for (int i = 1; i < 30; i++)
+            {
+                simple.Add(new Simpoles {Id=i,Name=$"第{i}个名称",Context=$"{i}的内容",CreatBy="郭利锋",CreateDate=DateTime.Now.AddDays(-i) });
+            }
+            return View(new SimpolesViewModels { simpoles = simple });
+        }
+    }
+}
---
+++ Sample/CoreTest/Models/Simpoles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTest.Models
+{
+    public class Simpoles
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public string Context { get; set; }
+        public string CreatBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ModfiDate { get; set; }
+    }
+}
